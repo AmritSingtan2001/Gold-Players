8021e60ed4276a834f596ef40d1d7886e7c889f6
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-17 16:39:03 +0545

    Feat(index):"desing update"

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path
 from . import views
+app_name ='app'
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blog', views.blog, name='blog'),
+    path('blog/detail', views.blog_detail, name='blog_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request,'app/index.html')
+
+
+def blog(request):
+    return render(request,'app/blog.html')
+
+def blog_detail(request):
+    return render(request,'app/blog-single.html')
