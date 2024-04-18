8443dfaae1999c6aa79b5eb7ce4bf3faa4faba43
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-18 17:43:05 +0545

    Feat:Whole page design complete

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blog', views.blog, name='blog'),
     path('blog/detail', views.blog_detail, name='blog_detail'),
+    path('about-us',views.aboutus, name='about'),
+    path('solution',views.solution, name='solution'),
+    path('solution/detail',views.solution_detail, name='solution_detail'),
+    path('sectors', views.sectors, name='sectors'),
+    path('sectors/detail', views.sector_detail, name='sector_detail'),
+    path('location', views.location, name='location'),
+    path('testimonials', views.testimonials, name='testimonials'),
+    path('careers', views.careers, name='careers')
 ]
--- app/views.py
+++ app/views.py
@@ -4,9 +4,33 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     return render(request,'app/index.html')
 
+def aboutus(request):
+    return render(request,'app/about.html')
 
 def blog(request):
     return render(request,'app/blog.html')
 
 def blog_detail(request):
     return render(request,'app/blog-single.html')
+
+def solution(request):
+    return render(request,'app/solutions.html')
+
+
+def solution_detail(request):
+    return render(request,'app/solution_detail.html')
+
+def sectors(request):
+    return render(request,'app/sectors.html')
+
+def sector_detail(request):
+    return render(request,'app/sectors_detail.html')
+
+def location(request):
+    return render(request,'app/location.html')
+
+def testimonials(request):
+    return render(request,'app/testimonials.html')
+
+def careers(request):
+    return render(request,'app/careers.html')
