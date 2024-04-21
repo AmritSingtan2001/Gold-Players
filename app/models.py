423dc90d99026dfd42618cf995caad4bae13cbbe
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-21 11:25:34 +0545

    Feat(Solution)"Solution Model Added"

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 ''' bannar model '''
 class Banner(models.Model):
@@ -7,4 +8,22 @@ class Banner(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+
+class Solutions(models.Model):
+    icon_image = models.ImageField(verbose_name='Solution Icon Image',upload_to='solutionimages/')
+    image = models.ImageField(verbose_name='Solution Main Image',upload_to='solutionimages/')
+    title = models.CharField(verbose_name='Solution Name',max_length=150)
+    descriptions = models.TextField(verbose_name='Enter your descriptions')
+
+    class Meta:
+        ordering =['-id']
+        verbose_name='Solution'
+        verbose_name_plural = 'Solutions'
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('solution-detail', kwargs={'pk': self.pk})
     
