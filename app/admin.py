e0aa7ce01ac517ffffd81b2a0bc8ee078817d873
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-21 11:27:02 +0545

    Feat("Solution Admin Panel"):"Solution register in admin panel"

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from . models import Banner
+from . models import Banner, Solutions
 
 class BannerAdmin(admin.ModelAdmin):
     model = Banner
     list_display =['id','bannar_image'] 
 admin.site.register(Banner, BannerAdmin)
+
+class SolutionAdmin(admin.ModelAdmin):
+    model = Solutions
+    list_display =['title','icon_image','image']
+admin.site.register(Solutions,SolutionAdmin)
