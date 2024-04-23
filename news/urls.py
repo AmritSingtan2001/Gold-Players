a1f745326fe320847dcb3271be67b5aafba4c35e
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-23 13:15:19 +0545

    Refactor(Base,Resources):Resources page dynamic complete and base resources url changes to resources

--- news/urls.py
+++ news/urls.py
@@ -1,13 +1,15 @@
 
 from django.urls import path
 from . import views
-from .views import ResourcseListView
+from .views import ResourcseListView,ResourceDetailView,NewsResourcseListView,InsightsResourcseListView,CaseStudiesResourcseListView
 app_name ='resources'
 
 
 urlpatterns = [
     path('', ResourcseListView.as_view(), name='all_resources'),
-    path('blog', views.blog, name='blog'),
-    path('blog/detail', views.blog_detail, name='blog_detail'),
-    
+    path('detail/<slug:slug>', ResourceDetailView.as_view(), name='resource_detail'),
+    path('news',NewsResourcseListView.as_view(), name='resources_news'),
+    path('insights',InsightsResourcseListView.as_view(), name='resources_insight'),
+    path('case-studies',CaseStudiesResourcseListView.as_view(), name='resources_case_studies'),
+
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from . models import Resources
 from django.views import generic
@@ -7,10 +9,43 @@ class ResourcseListView(generic.ListView):
     model = Resources
     template_name ='news/resources.html'
     context_object_name ='resources'
+    paginate_by =3
 
 
-def blog(request):
-    return render(request,'app/blog.html')
+class ResourceDetailView(generic.DetailView):
+    model = Resources 
+    template_name ='news/resource_detail.html'
+    context_object_name ='resource_detail'
+    slug_url_kwarg = 'slug' 
 
-def blog_detail(request):
-    return render(request,'app/blog-single.html')
+''' news related rescources '''
+class NewsResourcseListView(generic.ListView):
+    model = Resources
+    template_name ='news/resources.html'
+    context_object_name ='resources'
+    paginate_by =3
+
+    def get_queryset(self):
+        return Resources.objects.filter(resource_type='news')
+    
+
+''' insights related rescources '''
+class InsightsResourcseListView(generic.ListView):
+    model = Resources
+    template_name ='news/resources.html'
+    context_object_name ='resources'
+    paginate_by =3
+
+    def get_queryset(self):
+        return Resources.objects.filter(resource_type='insights')
+    
+
+''' case studies related rescources '''
+class CaseStudiesResourcseListView(generic.ListView):
+    model = Resources
+    template_name ='news/resources.html'
+    context_object_name ='resources'
+    paginate_by =3
+
+    def get_queryset(self):
+        return Resources.objects.filter(resource_type='case_studies')
