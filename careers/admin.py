8379875dac5face563fab4c8151c88953306b031
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-23 14:33:58 +0545

    Feat(Admin Panel):Career Model register on Admin panel

--- careers/admin.py
+++ careers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Careers
 
-# Register your models here.
+class CareersAdmin(admin.ModelAdmin):
+    model = Careers
+    list_display =['job_title','created_date','valid_date']
+admin.site.register(Careers, CareersAdmin)
