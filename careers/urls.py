01603c10c72994ba45ba4200c7407ac3d145bbd6
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-23 14:43:40 +0545

    Feat(Careers):Career App Business logic added.

--- careers/urls.py
+++ careers/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 from . import views
-from .views import *
+from .views import CareersListView
 app_name ='careers'
 
 
 urlpatterns = [
+    path('', CareersListView.as_view(), name='careers'),
 ]
--- careers/views.py
+++ careers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import Careers
+from django.views import generic
 
-# Create your views here.
+
+'''careers view '''
+class CareersListView(generic.ListView):
+    model = Careers
+    template_name ='careers/careers.html'
+    context_object_name= 'careers'
+
