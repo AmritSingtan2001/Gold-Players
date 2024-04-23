27b3029b2fac1f2ba80fa5f6fe1a2a123f3b11a1
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-23 15:18:08 +0545

    Refactor(Career Model):Career Model Field Change

--- careers/models.py
+++ careers/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from autoslug import AutoSlugField
 from ckeditor.fields import RichTextField
 
+''' career category'''
+class Category(models.Model):
+    category_name = models.CharField(verbose_name='Career Category Name' ,max_length=150)
+
+
+    def __str__(self):
+        return self.category_name
+    
+
+'''career model '''
 class Careers(models.Model):
-    image = models.ImageField(upload_to='careerimage/', verbose_name='Company Logo')
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True, verbose_name='Choose Career Category')
+    company_logo = models.ImageField(upload_to='careerimage/', verbose_name='Company Logo')
     job_title = models.CharField(verbose_name='Job Title', max_length=150)
     location = models.CharField(verbose_name='Location', max_length=150)
     salary = models.CharField(verbose_name='Salary', max_length=150, null=True, blank=True)
