be3fada6ca402a6e73266654fe289b52cc5fae54
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-24 14:30:09 +0545

    Refactor:Admin panel customization

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import About,Objective,OrganizationSetting
 
+admin.site.site_header = 'Gold Player'                   
+admin.site.index_title = 'Gold Player'                 
+admin.site.site_title = 'Gold Player'
+
+
+
 class OrganizationSettingAdmin(admin.ModelAdmin):
     model = OrganizationSetting
     list_display =['site_name','phone_number','email','logo']
