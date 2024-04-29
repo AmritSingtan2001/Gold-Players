bbb06643990202fa6d8f948404d4bbe512a27a45
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-29 15:42:00 +0545

    Refactor:Model label

--- careers/models.py
+++ careers/models.py
@@ -23,14 +23,14 @@ JOB_TYPE_CHOICES = [
 '''career model '''
 class Careers(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True, verbose_name='Choose Career Category')
-    company_logo = models.ImageField(upload_to='careerimage/', verbose_name='Company Logo')
-    job_title = models.CharField(verbose_name='Job Title', max_length=150)
-    location = models.CharField(verbose_name='Location', max_length=150)
-    salary = models.CharField(verbose_name='Salary', max_length=150, null=True, blank=True)
+    company_logo = models.ImageField(upload_to='careerimage/', verbose_name='Company Logo *')
+    job_title = models.CharField(verbose_name='Job Title *', max_length=150)
+    location = models.CharField(verbose_name='Location *', max_length=150)
+    salary = models.CharField(verbose_name='Salary *', max_length=150, null=True, blank=True)
     created_date = models.DateTimeField(auto_now=True)
-    valid_date = models.DateField(verbose_name='Valid Date')
-    job_type = models.CharField(verbose_name='Job Type', max_length=20, choices=JOB_TYPE_CHOICES, default='full_time')
-    descriptions = RichTextField(verbose_name='Job Descriptions(responsibility,qualification)')
+    valid_date = models.DateField(verbose_name='Valid Date *')
+    job_type = models.CharField(verbose_name='Job Type *', max_length=20, choices=JOB_TYPE_CHOICES, default='full_time')
+    descriptions = RichTextField(verbose_name='Job Descriptions(responsibility,qualification) *')
     slug = AutoSlugField(populate_from ='job_title', default=None, unique=True)
 
     class Meta:
